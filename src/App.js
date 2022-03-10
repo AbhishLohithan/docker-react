ed32687e4076c7e9de17b76485a8efa91136f684
Abhish Lohithan <abhish.lohithan@gmail.com>
2022-03-10 04:22:28 +0000

    modified app content in feature

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Abhish - Deployment using nginx, docker, travis and elasticbean
+          Abhish - Deployment using nginx, docker, travis and elasticbeanstalk 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>I was changed on the feature</p>
       </header>
     </div>
   );
